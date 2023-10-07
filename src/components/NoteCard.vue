8c40a53a612de3d4e6c6e99ad26b25671f4de469
cccaaannn <can.kurt.aa@gmail.com>
2023-10-08 01:18:56 +0300

    components updated

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -14,13 +14,20 @@ function onDelete() {
 
 <template>
     <div class="card">
+        <div class="card-header-container">
+            <div class="card-header">
+                <h3>{{ note.title }}</h3>
+                <h6 class="date">
+                    {{ note.date.toLocaleDateString() }}
+                </h6>
+            </div>
+
+            <button @click="onDelete">X</button>
+        </div>
+
         <p>
             {{ note.content }}
         </p>
-
-        <div>
-            <button @click="onDelete">X</button>
-        </div>
     </div>
 </template>
 
@@ -33,8 +40,13 @@ button:hover {
     border-color: var(--secondary);
 }
 
+.date {
+    color: var(--primary);
+}
+
 .card {
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
     border: 1px solid var(--primary);
     padding: 1em;
@@ -42,5 +54,18 @@ button:hover {
     word-wrap: break-word;
     word-break: break-all;
     border-radius: 10px;
+}
+
+.card-header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 5px;
 }
 </style>
--- src/components/NoteForm.vue
+++ src/components/NoteForm.vue
@@ -2,23 +2,30 @@
 import { Note } from '@/types/note'
 import { ref } from 'vue';
 
-const currentText = ref("")
+const currentContent = ref("")
+const currentTitle = ref("")
 
 const emit = defineEmits<{
     (e: 'add', note: Note): void
 }>()
 
 function onAdd() {
-    if (!currentText.value || currentText.value.trim() === "") {
+    if (
+        !currentTitle.value || currentTitle.value.trim() === "" ||
+        !currentContent.value || currentContent.value.trim() === ""
+    ) {
         return;
     }
 
     const newNote: Note = {
         id: crypto.randomUUID(),
-        content: currentText.value
+        title: currentTitle.value,
+        content: currentContent.value,
+        date: new Date()
     }
 
-    currentText.value = ""
+    currentContent.value = ""
+    currentTitle.value = ""
     emit('add', newNote)
 }
 
@@ -26,7 +33,12 @@ function onAdd() {
 
 <template>
     <div class="form">
-        <textarea autofocus v-model="currentText" rows="5" />
+        <label for="title">Title</label>
+        <input autofocus name="title" maxlength="20" v-model="currentTitle" />
+        
+        <label for="content">Content</label>
+        <textarea name="content" rows="5" v-model="currentContent" />
+
         <button type="button" @click="onAdd">+ADD</button>
     </div>
 </template>
@@ -34,6 +46,10 @@ function onAdd() {
 <style scoped>
 button {
     background-color: var(--primary);
+}
+
+input {
+    max-width: 200px;
 }
 
 .form {
